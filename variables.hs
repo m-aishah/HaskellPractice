80b539ae9ab6577dc16565c3e30e7b7b961c910d
Aishah <maishah2540.com>
2024-06-08 19:18:59 +0300

    Variables & Concatenation of strings.

---
+++ variables.hs
@@ -0,0 +1,7 @@
+main = do
+    let name = "Dudley"
+    -- Concatenation
+    putStrLn (name ++ " received 36 gifts for his birthday.")
+    putStrLn ("36 gifts? " ++ name ++ "thought out lout. But last year I had 37!")
+    putStrLn "This is when we knew."
+    putStrLn (name ++ " is a spoilt child.")
