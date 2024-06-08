a881eab08f17bb8b64c447009dcd0d8c4eb04962
Aishah <maishah2540.com>
2024-06-08 19:32:09 +0300

    Variables, show, concatenation

--- variables.hs
+++ variables.hs
@@ -1,7 +1,8 @@
 main = do
+    -- Variables, show, concatenation
+    let x = 36
     let name = "Dudley"
-    -- Concatenation
     putStrLn (name ++ " received 36 gifts for his birthday.")
-    putStrLn ("36 gifts? " ++ name ++ "thought out lout. But last year I had 37!")
+    putStrLn (show x ++ " gifts? " ++ name ++ "thought out lout. But last year I had " ++ show (x + 1) ++ "!")
     putStrLn "This is when we knew."
     putStrLn (name ++ " is a spoilt child.")
