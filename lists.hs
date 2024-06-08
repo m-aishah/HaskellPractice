52e49cd72f5c891d133129ba15fee8ed0887cf33
Aishah <maishah2540.com>
2024-06-08 21:10:47 +0300

    More lists - some built in functions

--- lists.hs
+++ lists.hs
@@ -4,3 +4,8 @@ scores = [79, 83, 100, 92, 56, 74]
 main = do
     -- !!: Bang bang operator.
     print (scores !! 2)
+    -- Built in Functions
+    print (head scores) -- First item of the list.
+    print (last scores) -- Last item in the list.
+    print (init scores) -- Everything in the list except the last element.
+    print (tail scores) -- Everything except the first item in the list.
