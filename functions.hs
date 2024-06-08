46f3225fa79df1dace1e602d26f1d262c7edb1c7
Aishah <maishah2540.com>
2024-06-08 21:35:14 +0300

    More on functions

--- functions.hs
+++ functions.hs
@@ -1,14 +1,15 @@
 -- Functions in Haskell are written in this form:
 -- fn args = <expr>
 
-sayHi :: IO () -- Function types
-sayHi = do
-    putStrLn "What is your name? "
-    name <- getLine
-    putStrLn ("Hello " ++ name ++ "!")
+sayHi :: String -> Integer -> IO () -- Function types, like the function signature.
+sayHi name age = 
+    putStrLn ("Hello " ++ name ++ "! You are " ++ show age ++ " years old.")
     
 main :: IO() -- Main function is an IO function.
 main = do -- do runs from top to bottom.
-    sayHi
-    sayHi
-    sayHi
+    putStr "What is your name? "
+    name <- getLine
+    putStr "How old are you? "
+    inputAge <- getLine
+    let age = read inputAge :: Integer
+    sayHi name age
