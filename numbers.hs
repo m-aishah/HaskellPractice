4e445141bf5170dd51be95f528f0ad75fe9471b7
Aishah <maishah2540.com>
2024-06-08 19:53:45 +0300

    Built in Math functions

--- numbers.hs
+++ numbers.hs
@@ -1,5 +1,8 @@
 main = do
-    print (3 + 2)
-    print (5 - 4)
-    print (3 * 2)
-    print (4 / 4)
+    -- Power, built in math functions
+    print (3 ** 2)
+    print (sqrt 36) -- Square root.
+    print (round 3.6) -- Round Up.
+    print (round 3.3) -- Round Down.
+    print (floor 3.9) -- Floor: aLways round DOWN to the next integer.
+    print (ceiling 3.1) -- Ceiling: Always round UP to the next integer
